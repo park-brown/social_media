da9321c4309d261404ac682672ba12d8d704e959
Park cloud <340688748@qq.com>
2021-07-14 12:52:59 +0800

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+const app = express();
+dotenv.config();
+
+//connnect to mongo
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+	console.log('Connected to MongoDB');
+});
+
+// middleware
+app.use(express.json());
+app.use(helmet);
+app.use(morgan('common'));
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
+//listen
+app.listen(8800, () => {
+	console.log('backend server is ready ');
+});
